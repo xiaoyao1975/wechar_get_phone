6b26c751249b4fd8a4ac35c72e62a21cd5bf57f3
xiaoyao <18602222296@163.com>
2021-06-10 06:31:29 +0800

    update 2021-06-10

--- src/main/java/com/itany/servlt/HelloServlet.java
+++ src/main/java/com/itany/servlt/HelloServlet.java
@@ -1,10 +1,12 @@
 package com.itany.servlt;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/hello")
@@ -12,7 +14,14 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.service(req, resp);
-        req.setAttribute("name","tom");
-        req.getRequestDispatcher("index.jsp").forward(req,resp);
+        req.setCharacterEncoding("utf8");
+        String sql=req.getParameter("sql");
+        HttpSession session = req.getSession();
+        session.setAttribute("usr","hello 你好");
+        //将页面转发到欢迎页面
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/welcome.jsp");
+        requestDispatcher.forward(req,resp);
+
+
     }
 }
